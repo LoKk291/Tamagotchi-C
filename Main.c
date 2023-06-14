9a8febf862e4da571f21f13ca41065e6f771e6c7
LoKk291 <davidvegah291@gmail.com>
2023-06-14 20:06:07 -0300

    Prueba de lectura del archivo

--- Main.c
+++ Main.c
@@ -2,22 +2,28 @@
 #include <stdlib.h>
 #include "libraries/colors.h"
 
-//esta función carga las configuraciones PRINCIPALES
-int assetsLoad(char petName[N]){
+//esta funciï¿½n carga las configuraciones PRINCIPALES
+int assetsLoad(char petName[]){
     char ruta[] = "../files/assets.txt"; //se vuelve una carpeta para atras para que el .exe pueda abrir el archivo
+    char caracter;
+
 	// el archivo assets contiene las configuraciones PRONCIPALES
-    FILE *fileAssets = fopen(ruta, "r");
+    FILE* fileAssets = fopen(ruta, "rb");     //se habre en modo lectura tipo binario
     if(fileAssets == NULL){
         printf("ERROR FATAL, no se ha podido abrir el archivo 'assets'\n");
         return 1;
     }
     
+    while(feof(fileAssets) == 0){
+        caracter = fgetc(fileAssets);
+        printf("%c ", caracter);
+    }
     return 0;
 }
 	
 int main(int argc, char *argv[]){
-    char petName[];
-    char userName[];
+    char petName;
+    char userName;
     int gameAvatar;
     int petInmortality;
     int gameDifficult;
