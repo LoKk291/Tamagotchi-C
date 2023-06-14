13435bb4ccd7b6e278b97def4587e9fcaaf1d818
LoKk291 <davidvegah291@gmail.com>
2023-06-14 10:50:07 -0300

    Fixeo de assetsLoad()

--- Main.c
+++ Main.c
@@ -1,18 +1,26 @@
 #include <stdio.h>    
+#include <stdlib.h>
 #include "libraries/colors.h"
 
 int assetsLoad(){
-    FILE* fileAssets = fopen("files/assets.txt", "r");
+    char ruta[] = "../files/assets.txt"; //se vuelve una carpeta para atras para que el .exe pueda abrir el archivo
+    FILE *fileAssets = fopen(ruta, "r");
     if(fileAssets == NULL){
         printf("ERROR FATAL, no se ha podido abrir el archivo 'assets'\n");
         return 1;
     }
+
     return 0;
 }
 
 int main(){
-    if(assetsLoad() != 0){
-        return 1;
-    }
+    char petName[] = "Mojojo";
+    char userName[] = "LoKk";
+    int gameAvatar = 1;
+    int petInmortality = 1;
+    int gameDifficult = 0;
+
+    assetsLoad();
+    system("pause");
     return 0;
 }
