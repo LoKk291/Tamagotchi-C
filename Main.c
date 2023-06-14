5b8fbc47125eb464159ecc32722a26d81cb5b59d
LoKk291 <davidvegah291@gmail.com>
2023-06-14 09:59:51 -0300

    Función assetsLoad
    Esta función abre el archivo con las configuraciones principales

--- Main.c
+++ Main.c
@@ -1,7 +1,18 @@
 #include <stdio.h>    
 #include "libraries/colors.h"
 
+int assetsLoad(){
+    FILE* fileAssets = fopen("files/assets.txt", "r");
+    if(fileAssets == NULL){
+        printf("ERROR FATAL, no se ha podido abrir el archivo 'assets'\n");
+        return 1;
+    }
+    return 0;
+}
+
 int main(){
-        
+    if(assetsLoad() != 0){
+        return 1;
+    }
     return 0;
 }
