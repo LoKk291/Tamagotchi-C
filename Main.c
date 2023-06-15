ac1b580fc376b3deff5a3c4bac87f35ad0755928
LoKk291 <davidvegah291@gmail.com>
2023-06-15 19:09:48 -0300

    Implementación struct dataStateBars
    Contiene los datos de las barras de estado

--- Main.c
+++ Main.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "libraries/colors.h"
-#define N 12
+#define N 13
 
-// esta estructura contiene la información PRINCIPAL en timepo de ejecución
+// esta estructura contiene la información PRINCIPAL en tiempo de ejecución
 struct AssetsData
 {
+    //tanto el nombre de la mascomo como de usuario seran de maximo 12 caracteres
     char petName[N];
     char userName[N];
     int gameAvatar;
@@ -13,7 +14,13 @@ struct AssetsData
     int gameDifficult;
 };
 
-// esta funci�n carga las configuraciones PRINCIPALES
+struct dataStateBars{
+    int health;
+    int mood;
+    int hungry;
+};
+
+// esta funcion carga las configuraciones PRINCIPALES
 int assetsLoad(struct AssetsData **ptrAssetsData)
 {
     char ruta[] = "../files/assets.txt"; // se vuelve una carpeta para atras para que el .exe pueda abrir el archivo
@@ -30,6 +37,10 @@ int assetsLoad(struct AssetsData **ptrAssetsData)
     fflush(stdin);
     fscanf(fileAssets, "%s%s%i%i%i", &(*ptrAssetsData)->userName, &(*ptrAssetsData)->petName, &(*ptrAssetsData)->gameAvatar, &(*ptrAssetsData)->petInmortality, &(*ptrAssetsData)->gameDifficult);
     fclose(fileAssets); //terminada la carga del struct se ciera el archivo
+    return 0;
+}
+
+int stateBars(){
     
     return 0;
 }
@@ -37,8 +48,13 @@ int assetsLoad(struct AssetsData **ptrAssetsData)
 int main()
 {
     struct AssetsData* ptrAssetsData = (struct AssetsData*)malloc(sizeof(struct AssetsData)); //se le asigna un espacio en memoria a la estructura
+    struct dataStateBars* ptrDataStateBars = (struct dataStateBars*)malloc(sizeof(struct dataStateBars));
 
-    assetsLoad(&ptrAssetsData);
+    if(assetsLoad(&ptrAssetsData)){
+        printf("ERROR FATAL, NO SE CARGARON LOS DATOS PRINCIPALES...");
+        return 1;
+    }
+
     system("pause");
     return 0;
 }
