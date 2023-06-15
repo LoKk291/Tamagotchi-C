14cc7de0bea93202772ba22ce17e4ab1432952c6
LoKk291 <davidvegah291@gmail.com>
2023-06-14 22:17:08 -0300

    Pruebas de lectura del archivo Assets.txt

--- Main.c
+++ Main.c
@@ -1,34 +1,45 @@
-#include <stdio.h>    
+#include <stdio.h>
 #include <stdlib.h>
 #include "libraries/colors.h"
+#define N 12
 
-//esta funci�n carga las configuraciones PRINCIPALES
-int assetsLoad(char petName[]){
-    char ruta[] = "../files/assets.txt"; //se vuelve una carpeta para atras para que el .exe pueda abrir el archivo
-    char caracter;
-
-	// el archivo assets contiene las configuraciones PRONCIPALES
-    FILE* fileAssets = fopen(ruta, "rb");     //se habre en modo lectura tipo binario
-    if(fileAssets == NULL){
-        printf("ERROR FATAL, no se ha podido abrir el archivo 'assets'\n");
-        return 1;
-    }
-    
-    while(feof(fileAssets) == 0){
-        caracter = fgetc(fileAssets);
-        printf("%c ", caracter);
-    }
-    return 0;
-}
-	
-int main(int argc, char *argv[]){
-    char petName;
-    char userName;
+// esta estructura contiene la información PRINCIPAL en timepo de ejecución
+struct AssetsData
+{
+    char petName[N];
+    char userName[N];
     int gameAvatar;
     int petInmortality;
     int gameDifficult;
+};
 
-    assetsLoad(&petName);
+// esta funci�n carga las configuraciones PRINCIPALES
+int assetsLoad(struct AssetsData **assetsData)
+{
+    char ruta[] = "../files/assets.txt"; // se vuelve una carpeta para atras para que el .exe pueda abrir el archivo
+
+    // el archivo assets contiene las configuraciones PRONCIPALES
+    FILE *fileAssets = fopen(ruta, "rb"); // se habre en modo lectura tipo binario
+    if (fileAssets == NULL)
+    {
+        printf("ERROR FATAL, no se ha podido abrir el archivo 'assets'\n");
+        return 1;
+    }
+
+    while (feof(fileAssets) == 0)
+    {
+        fflush(stdin);
+        fscanf(fileAssets, "%c", &(*assetsData)->petName);
+    }
+    printf("%s", (*assetsData)->petName);
+    return 0;
+}
+
+int main()
+{
+    struct AssetsData* assetsData = (struct AssetsData*)malloc(sizeof(struct AssetsData));
+
+    assetsLoad(&assetsData);
     system("pause");
     return 0;
 }
