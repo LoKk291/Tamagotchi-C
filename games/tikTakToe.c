233a0d837beae83cae91366f68d1a98366d41cd9
Albers <Albers@aptugo.com>
2023-06-20 18:20:01 -0300

    parte básica del juego

--- games/tikTakToe.c
+++ games/tikTakToe.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <time.h>
 
+#define USER 'X'
+#define PET 'O'
+
+#define R 3
+#define C 3
+
+int main(){
+    //el campo del juego será una matriz
+    char board[R][C];
+    char optAgain;
+
+    do{
+        printf("Desea volver a jugar: ");
+        scanf("%c", &optAgain);
+    }while(optAgain != 'S' && optAgain != 's');
+}
