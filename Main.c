343064e4ec02dd200388d5d1eef4e72e21573329
LoKk291 <davidvegah291@gmail.com>
2023-06-22 20:11:51 -0300

    cambio en la funcion showAndIncrementerStateBars
    ahora dependiendo del puntaje cambia el color de la barra.
    70 para arriba verde, 69 hasta 40 amarillo, 39 hacia abajo rojo)

--- Main.c
+++ Main.c
@@ -217,7 +217,16 @@ void showAndIncrementerStateBars(struct dataStateBars **ptrDataStateBars)
     // caracter de tabla ascii extendida
     char icon = 35; // 35 = #
 
-    // estos for dibujan las barras
+    // estos for dibujan las barras y los if determinan el color de la barra
+    //salud
+    if((*ptrDataStateBars)->health >= 70){
+        printf(GREEN);
+    }else if(((*ptrDataStateBars)->health <= 69) && ((*ptrDataStateBars)->health >= 40)){
+        printf(YELLOW);
+    }else{
+        printf(RED);
+    }
+    
     printf("Salud: ");
     for (int i = 0; i < ((*ptrDataStateBars)->health) / 4; i++)
     {
@@ -225,6 +234,14 @@ void showAndIncrementerStateBars(struct dataStateBars **ptrDataStateBars)
     }
     printf(" %i/100\n", (*ptrDataStateBars)->health);
 
+    //animo
+    if((*ptrDataStateBars)->mood >= 70){
+        printf(GREEN);
+    }else if(((*ptrDataStateBars)->mood <= 69) && ((*ptrDataStateBars)->hungry >= 40)){
+        printf(YELLOW);
+    }else{
+        printf(RED);
+    }
     printf("Animo: ");
     for (int i = 0; i < ((*ptrDataStateBars)->mood) / 4; i++)
     {
@@ -232,12 +249,22 @@ void showAndIncrementerStateBars(struct dataStateBars **ptrDataStateBars)
     }
     printf(" %i/100\n", (*ptrDataStateBars)->mood);
 
+    //hambre
+    if((*ptrDataStateBars)->hungry >= 70){
+        printf(GREEN);
+    }else if(((*ptrDataStateBars)->hungry <= 69) && ((*ptrDataStateBars)->hungry >= 40)){
+        printf(YELLOW);
+    }else{
+        printf(RED);
+    }
     printf("Hambre: ");
     for (int i = 0; i < ((*ptrDataStateBars)->hungry) / 4; i++)
     {
         printf("%c", icon);
     }
     printf(" %i/100\n", (*ptrDataStateBars)->hungry);
+
+    printf(RESET);
 }
 
 // arroja frases random contenidas en el archivo "phrases.txt"
