e9bcce427f20bb7df0a32c7bcf624e2dbb0cf099
Agustin Borges <tinnborges@gmail.com>
2023-06-25 11:15:53 -0300

    Implematación de comentarios explicativos y generación de semillas aleatorias

--- games/triviaMind.c
+++ games/triviaMind.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
-#define MAX_LINE_LENGTH 100
+/*
+MAX_LINE_LENGTH:represnta la longitud máxima de una linea en el archivo 
+.csv que contiene las preguntas y respuestas
+AMOUNT:indica la cantidad de preguntas que se mostraran en cada sesión.
+*/
+#define MAX_LINE_LENGTH 500
 #define AMOUNT 10
 
 // estructura donde guardar de forma individual, la pregunta actual y su respuesta (0= falso, 1=verdadero)
@@ -13,7 +19,7 @@ struct question
 };
 
 /*con esta funcion, separo en cada linea, las preguntas de las respuestas de cada linea leida;
-con la funcion strtok separo una linea usando un delimitador*/
+La funcion strlok divide la cadena en subcadenas utilizando un delimitaodr específico*/
 void separateQuestionsAnswers(char *line, struct question *question)
 {
     char *token = strtok(line, ";");
@@ -23,19 +29,24 @@ void separateQuestionsAnswers(char *line, struct question *question)
         token = strtok(NULL, ",");
         if (token != NULL)
         {
+            //copio la pregunta en la estructura y convierto la respuesta a un entero utilizando atoi();
             question->answer = atoi(token);
         }
     }
 }
 
-/*Funcion que genera un numero aleatorio dentro de un rango determinado*/
+/*Funcion que genera un numero aleatorio dentro de un rango determinado entre min y max*/
 int generateRandomNumber(int min,int max){
 return min + rand() % (max - min + 1);
 }
 
 int main(void)
 {
+    //utilizo la funcion time dentro de srand asi defino una semilla aleatoria distinta en cada ejecucion del programa
+    srand(time(NULL));
+
     printf("---------------Trivia Mind-------------\n");    
+   
     FILE *archive = fopen("../../files/triviaMind.csv","r"); // aca va la direccion del archivo
 
     if (archive == NULL)
@@ -44,12 +55,13 @@ int main(void)
         return 1;
     }
 
-    // generacion de variables necesarias para la implementacion del conteo de puntos y preguntas
+    // generacion de estructuras y variables necesarias para la implementacion del conteo de puntos y preguntas
     struct question question[AMOUNT];
     int totalquestions = 0;
     char line[MAX_LINE_LENGTH];
 
-    // separar linea actual y sumar 1 pregunta
+    // Leo las preguntas y respuestas del archivo y las almaceno en el arreglo 'question'
+   
     while (fgets(line, sizeof(line), archive))
     {
         separateQuestionsAnswers(line, &question[totalquestions]);
@@ -58,7 +70,9 @@ int main(void)
 
     fclose(archive);
 
-    //permutar las preguntas aleatoriamente
+    //permutar las preguntas aleatoriamente 
+    /*utilizo el algoritmo de Fisher-Yates:Comienzo en la ultima posicion del arreglo y en cada iteracion
+    intercambio la pregunta actual con una pregunta aleatoria anterior a ella.*/
     for (int i = totalquestions-1; i>0 ; i--)
     {
         int j= generateRandomNumber(0,i);
@@ -68,7 +82,10 @@ int main(void)
     }
 
 
-    //mostrar solo las primeras 10 preguntas permutadas
+    //mostrar solo las primeras 10 preguntas permutadas y solicitamos al usuario que ingrese sus respuestas
+   
+     /*operacio ternaria que asegura de que no se muestren mas preguntas de las disponibles en el archivo .csv;
+    o, en todo caso, si hay menos preguntas, muestre todas las disponibles*/
     int numQuestionsToShow = (totalquestions<AMOUNT) ? totalquestions : AMOUNT;
     int points=0;
     for(int i =0;i<numQuestionsToShow;i++)
@@ -83,15 +100,17 @@ int main(void)
         if(answer == question[i].answer)
         {
             printf("¡Respuesta correcta!\n");
-            points++;
+            points = points + 5;
         }else
         {
             printf("Respuesta incorrecta.\n");
+            points = points - 5;
         }
         printf("\n");
 
     }
 
+    //muuestra de puntaje final
     printf("\nPuntaje final: %d/%d\n", points, numQuestionsToShow);
 
     return 0;
