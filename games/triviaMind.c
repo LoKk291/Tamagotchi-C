f5c1008c4eb5147350c8e47c310cdac72514ca6e
Agustin Borges <tinnborges@gmail.com>
2023-06-25 10:40:58 -0300

    Inplementacion de aleatoriedad de preguntas en minijuego triviaMind

--- games/triviaMind.c
+++ games/triviaMind.c
@@ -2,13 +2,13 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define MAX_LINE_LENGHT 100
-#define CANT 10
+#define MAX_LINE_LENGTH 100
+#define AMOUNT 10
 
 // estructura donde guardar de forma individual, la pregunta actual y su respuesta (0= falso, 1=verdadero)
 struct question
 {
-    char question[MAX_LINE_LENGHT];
+    char question[MAX_LINE_LENGTH];
     int answer;
 };
 
@@ -28,6 +28,11 @@ void separateQuestionsAnswers(char *line, struct question *question)
     }
 }
 
+/*Funcion que genera un numero aleatorio dentro de un rango determinado*/
+int generateRandomNumber(int min,int max){
+return min + rand() % (max - min + 1);
+}
+
 int main(void)
 {
     printf("---------------Trivia Mind-------------\n");    
@@ -40,9 +45,9 @@ int main(void)
     }
 
     // generacion de variables necesarias para la implementacion del conteo de puntos y preguntas
-    struct question question[CANT];
+    struct question question[AMOUNT];
     int totalquestions = 0;
-    char line[MAX_LINE_LENGHT];
+    char line[MAX_LINE_LENGTH];
 
     // separar linea actual y sumar 1 pregunta
     while (fgets(line, sizeof(line), archive))
@@ -53,24 +58,41 @@ int main(void)
 
     fclose(archive);
 
-    int points = 0;
-    for (int i = 0; i < totalquestions; i++)
+    //permutar las preguntas aleatoriamente
+    for (int i = totalquestions-1; i>0 ; i--)
     {
-        int answer;
-        scanf("&d", &answer);
+        int j= generateRandomNumber(0,i);
+        struct question temp= question[i];
+        question[i]=question[j];
+        question[j]=temp;
+    }
 
-        if (answer == question[i].answer)
+
+    //mostrar solo las primeras 10 preguntas permutadas
+    int numQuestionsToShow = (totalquestions<AMOUNT) ? totalquestions : AMOUNT;
+    int points=0;
+    for(int i =0;i<numQuestionsToShow;i++)
+    {
+        printf("Pregunta %d: %s\n", i+1, question[i].question);
+
+        int answer;
+        
+        printf("Respuesta (0 = Falso, i = Verdadero): ");
+        scanf("%d",&answer);
+
+        if(answer == question[i].answer)
         {
-            printf("¡Respuesta correcta!.\n");
-        }
-        else
+            printf("¡Respuesta correcta!\n");
+            points++;
+        }else
         {
             printf("Respuesta incorrecta.\n");
         }
         printf("\n");
+
     }
 
-    printf("\nPuntaje final: %d/%d\n", points, totalquestions);
+    printf("\nPuntaje final: %d/%d\n", points, numQuestionsToShow);
 
     return 0;
 }
